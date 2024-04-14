2047fe1cbd1f2f657602a4131b6106476bdb325c
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-04-14 17:54:37 +0200

    OOP

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
-let reset = document.querySelector('#reset');
-let check = document.querySelector('#check');
-let input = document.querySelector('input');
-let result = document.querySelector('#result');
-let guess = document.querySelector('#guesses'); 
+class NumberGuess{
 
-let numbergenerator = () => Math.floor(Math.random() * 100);
-let number = 0;
-let guesses = 0;
-
-window.onload = number = numbergenerator();
-
-check.addEventListener('click', () => {
-    if(input.value != "")
-    {
-        let inputnumber = parseInt(input.value);
-        if(inputnumber != null && inputnumber == number)
-        {
-            result.innerHTML = `Congratulations!`;
-            result.style.color = 'green'; 
-            guesses++; guess.innerHTML = `Number of attempts: ${guesses}`; 
-        }
-        else if(inputnumber < number)
-        {
-            result.innerHTML = `Wrong number! Your number is too low!`;
-            result.style.color = 'red'; 
-            guesses++; guess.innerHTML = `Number of attempts: ${guesses}`;
-        }
-        else if(inputnumber > number)
-        {
-            result.innerHTML = `Wrong number! Your number is too high!`;
-            result.style.color = 'red'; 
-            guesses++; guess.innerHTML = `Number of attempts: ${guesses}`; 
-        }
+    constructor(){
+        this.reset = document.querySelector('#reset');
+        this.check = document.querySelector('#check');
+        this.input = document.querySelector('input');
+        this.result = document.querySelector('#result');
+        this.guess = document.querySelector('#guesses');
+        this.number = 0;
+        this.guesses = 0;
+        this.numbergenerator();
+        this.eventListeners();
     }
-    else
-        alert('Type number!');
-})
 
-reset.addEventListener('click', () => {
-    number = numbergenerator();
-    result.innerHTML = "";
-    input.value = "";
-    guess.innerHTML = "";
-    guesses = 0;
-})
+    numbergenerator = () => this.number = Math.floor(Math.random() * 100);
+
+    eventListeners = () =>{
+        this.check.addEventListener('click', () => {
+            if(this.input.value != "")
+            {
+                let inputnumber = parseInt(this.input.value);
+                if(inputnumber != null && inputnumber == this.number)
+                {
+                    this.result.innerHTML = `Congratulations!`;
+                    this.result.style.color = 'green'; 
+                    this.guesses++; this.guess.innerHTML = `Number of attempts: ${this.guesses}`; 
+                }
+                else if(inputnumber < this.number)
+                {
+                    this.result.innerHTML = `Wrong number! Your number is too low!`;
+                    this.result.style.color = 'red'; 
+                    this.guesses++; this.guess.innerHTML = `Number of attempts: ${this.guesses}`;
+                }
+                else if(inputnumber > this.number)
+                {
+                    this.result.innerHTML = `Wrong number! Your number is too high!`;
+                    this.result.style.color = 'red'; 
+                    this.guesses++; this.guess.innerHTML = `Number of attempts: ${this.guesses}`; 
+                }
+            }
+            else
+                alert('Enter number!');
+        })
+
+        this.reset.addEventListener('click', () => {
+            this.numbergenerator();
+            this.result.innerHTML = "";
+            this.input.value = "";
+            this.guess.innerHTML = "";
+            this.guesses = 0;
+        })
+    }
+}
+
+const game1 = new NumberGuess();
